7da0a02e244fc45380a0cf6cd0835862c93fd54f
double-beep <38133098+double-beep@users.noreply.github.com>
2018-06-16 09:23:08 +0300

    Another version

--- setup.py
+++ setup.py
@@ -1,34 +1,35 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-
-def readme():
-    with open('README.rst') as f:
-        return f.read()
-
-
-setup(name='funniest',
-      version='0.1',
-      description='The funniest joke in the world',
-      long_description=readme(),
-      classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Text Processing :: Linguistic',
+setup(name='python-package',
+      version='0.0',
+      description='Example setup.py',
+      url='httos://github.com/example/python-package',
+      author='Dependabot',
+      scripts=[],
+      packages=find_packages(),
+      setup_requires=[
+          'numpy==1.11.0',
+          'pytest-runner',
       ],
-      keywords='funniest joke comedy flying circus',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
-      license='MIT',
-      packages=['funniest'],
       install_requires=[
-          'markdown',
+          'boto3==1.3.1',
+          'flake8 > 2.5.4, < 3.0.0',
+          'gocardless_pro',
+          'pandas==0.19.2',
+          'pep8==1.7.0',
+          'psycopg2==2.6.1',
+          'raven == 5.32.0',
+          'requests==2.12.*',
+          'scipy==0.18.1',
+          'scikit-learn==0.18.1',
       ],
-      test_suite='nose.collector',
-      tests_require=['nose', 'nose-cover3'],
-      entry_points={
-          'console_scripts': ['funniest-joke=funniest.command_line:main'],
-      },
-      include_package_data=True,
-      zip_safe=False)
+      tests_require=[
+          'pytest==2.9.1',
+          'responses==0.5.1',
+      ],
+      extras_require=dict(
+          API=[
+              'flask==0.12.2',
+          ],
+      ),
+)
